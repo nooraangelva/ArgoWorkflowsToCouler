32d4856231e1f652c99223a2a17849be125be888
nooraangelva <78410623+nooraangelva@users.noreply.github.com>
2021-05-31 08:22:30 +0200

    Update map_diamond.py

--- map_diamond.py
+++ map_diamond.py
@@ -24,40 +24,30 @@ from couler.core.syntax.loop import map
 import couler.argo as couler
 from couler.argo_submitter import ArgoSubmitter
 
+def echo_trice(lista,letter):
+    couler.map(lambda x: echo(x, letter), lista)
 
-
-def echo_a(message):
+def echo(message, letter):
     return couler.run_container(
-        image="docker/whalesay:latest", command=["cowsay"], args=[message], step_name="A",
+        image="docker/whalesay:latest", command=["cowsay"], args=[letter+message], step_name=letter,
     )
 
-def echo_b(message):
-    return couler.run_container(
-        image="docker/whalesay:latest", command=["cowsay"], args=[message], step_name="B",
-    )
-
-def echo_c(message):
-    return couler.run_container(
-        image="docker/whalesay:latest", command=["cowsay"], args=[message], step_name="C",
-    )
-
-def echo_d(message):
-    return couler.run_container(
-        image="docker/whalesay:latest", command=["cowsay"], args=[message], step_name="D",
-    )
 
 def map_diamond():
     '''main function (the brains) creates dependencies and calls functions '''
         
-    lista_a=["A1","A2","A3"]
-    lista_b=["B1","B2","B3"]
-    lista_c=["C1","C2","C3"]
-    lista_d=["D1","D2","D3"]
+    lista_a=["1","2","3"]
+    
+    #lista_b=["B1","B2","B3"]
+    #lista_c=["C1","C2","C3"]
+    #lista_d=["D1","D2","D3"]
 
-    couler.set_dependencies(lambda: couler.map(lambda x: echo_a(x), lista_a), dependencies=None)
-    couler.set_dependencies(lambda: couler.map(lambda x: echo_b(x), lista_b), dependencies=["A"])
-    couler.set_dependencies(lambda: couler.map(lambda x: echo_c(x), lista_c), dependencies=["A"])
-    couler.set_dependencies(lambda: couler.map(lambda x: echo_d(x), lista_d), dependencies=["B","C"])
+    couler.set_dependencies(lambda: echo_trice(lista_a, "A"), dependencies=None)
+    couler.set_dependencies(lambda: echo_trice(lista_a, "B"), dependencies=["A"])
+    couler.set_dependencies(lambda: echo_trice(lista_a, "C"), dependencies=["A"])
+    couler.set_dependencies(lambda: echo_trice(lista_a, "D"), dependencies=["B","C"])
+
+
 
 map_diamond()
 
