02b98e73eb007fa5c9c51c41f488276424e53235
nooraangelva <78410623+nooraangelva@users.noreply.github.com>
2021-07-13 14:16:50 +0200

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,62 +1,101 @@
+apiVersion: argoproj.io/v1alpha1
+kind: Workflow
+metadata:
+  generateName: kati-
+spec:
+  entrypoint: boss
+  volumeClaimTemplates:
+  - metadata:
+      name: workdir
+    spec:
+      accessModes: ["ReadWriteMany"]
+      resources:
+        requests:
+          storage: 1Gi
+  volumes:
+  - name: cloud-pass
 
-# Couler version:
-import couler.argo as couler
-from couler.argo_submitter import ArgoSubmitter
-from couler.core.templates.volume import VolumeMount, Volume
-from couler.core.templates.volume_claim import VolumeClaimTemplate
-from couler.core.syntax.volume import create_workflow_volume
+
+  templates:
+  - name: boss
+    dag:
+      tasks:
+      - name: first-step
+        template: first-step-template
+      - name: second-first-way-step
+        dependencies: [first-step]
+        template: second-step-first-template
+      - name: second-second-way-step
+        dependencies: [first-step]
+        template: second-step-two-template
 
 
-def Step(volume_mount):
-    '''Generate '''
+  - name: first-step-template
+    script:
+      image: cmsopendata/cmssw_5_3_32
+      command: [bash]
+      source: | 
+        source /opt/cms/entrypoint.sh
+        git clone git://github.com/cms-legacydata-analyses/PhysObjectExtractorTool.git
+        cd PhysObjectExtractorTool
+        cd PhysObjectExtractor
+        scram b
+        ln -s python/poet_cfg.py .
+        ln -sf /cvmfs/cms-opendata-conddb.cern.ch/FT_53_LV5_AN1_RUNA FT_53_LV5_AN1
+        ln -sf /cvmfs/cms-opendata-conddb.cern.ch/FT_53_LV5_AN1_RUNA.db FT_53_LV5_AN1_RUNA.db
+        cmsRun poet_cfg.py
+        cp myoutput.root /mnt/vol/
+        ls -a  /mnt/vol/
+      volumeMounts:
+      - name: workdir
+        mountPath: /mnt/vol
 
-    couler.run_container(
-        image="cmsopendata/cmssw_5_3_32:latest",
-        command=["bash","-c"],
-        args=[
-            "source /opt/cms/entrypoint.sh; git clone git://github.com/cms-legacydata-analyses/PhysObjectExtractorTool.git; cd PhysObjectExtractorTool; cd PhysObjectExtractor; scram b;  ln -s python/poet_cfg.py .; ln -sf /cvmfs/cms-opendata-conddb.cern.ch/FT_53_LV5_AN1_RUNA FT_53_LV5_AN1; ln -sf /cvmfs/cms-opendata-conddb.cern.ch/FT_53_LV5_AN1_RUNA.db FT_53_LV5_AN1_RUNA.db; cmsRun poet_cfg.py; cp myoutput.root /mnt/vol/",
-        ],
-        volume_mounts=[volume_mount],
-        step_name="first"
-    )
+  - name: second-step-first-template
+    script:
+      image: nooraangelva/cmssw:10_6_12-argo-v2
+      command: [bash]
+      source: | 
+        source /opt/cms/cmsset_default.sh
+        cd $HOME/CMSSW_10_6_12/src
+        eval `scramv1 runtime -sh`
 
-def Stepone(volume_mount):
-    '''Generate '''
+        git clone https://github.com/cms-legacydata-analyses/PhysObjectExtractorTool.git
+        cd PhysObjectExtractorTool
+        cd PhysObjectExtractor
+        cd test
+        \cp -r /mnt/vol/myoutput.root ./
+        g++ -g -O3 -Wall -Wextra -o EventLoopAnalysis EventLoopAnalysisTemplate.cxx $(root-config --cflags --libs)
+        ./EventLoopAnalysis
+        ls -a
 
-    couler.run_container(
-        image="nooraangelva/cmssw:10_6_12-argo-v2",
-        command=["bash","-c"],
-        args=[
-            "source /opt/cms/cmsset_default.sh; cd $HOME/CMSSW_10_6_12/src; eval `scramv1 runtime -sh`; git clone https://github.com/cms-legacydata-analyses/PhysObjectExtractorTool.git; cd PhysObjectExtractorTool; cd PhysObjectExtractor; cd test; \cp -r /mnt/vol/myoutput.root ./; g++ -g -O3 -Wall -Wextra -o EventLoopAnalysis EventLoopAnalysisTemplate.cxx $(root-config --cflags --libs); ./EventLoopAnalysis; ls -a",
-        ],
-        volume_mounts=[volume_mount],
-    )
+      volumeMounts:
+      - name: workdir
+        mountPath: /mnt/vol
 
-def Steptwo(volume_mount):
-    '''Generate '''
+  - name: second-step-two-template
+    script:
+      image: nooraangelva/cmssw:10_6_12-argo-v2
+      command: [bash]
+      source: | 
+        source /opt/cms/cmsset_default.sh
+        cd $HOME/CMSSW_10_6_12/src
+        eval `scramv1 runtime -sh`
 
-    couler.run_container(
-        image="nooraangelva/cmssw:10_6_12-argo-v2",
-        command=["bash","-c"],
-        args=[
-            "source /opt/cms/cmsset_default.sh; cd $HOME/CMSSW_10_6_12/src; eval `scramv1 runtime -sh`; git clone https://github.com/cms-legacydata-analyses/PhysObjectExtractorTool.git; cd PhysObjectExtractorTool; cd PhysObjectExtractor; cd test; \cp -r /mnt/vol/myoutput.root ./; g++ -g -O3 -Wall -Wextra -Wpedantic -o RDFAnalysis RDFAnalysisTemplate.cxx $(root-config --cflags --libs); ./RDFAnalysis; ls -a",
-        ],
-        volume_mounts=[volume_mount],
-    )
-   
-def opendata():
-    '''creates volume and executes containers by calling functions (containers)'''
+        git clone https://github.com/cms-legacydata-analyses/PhysObjectExtractorTool.git
+        cd PhysObjectExtractorTool
+        cd PhysObjectExtractor
+        cd test
+        \cp -r /mnt/vol/myoutput.root ./
+        g++ -g -O3 -Wall -Wextra -Wpedantic -o RDFAnalysis RDFAnalysisTemplate.cxx $(root-config --cflags --libs)
+        ./RDFAnalysis
+        ls -a
 
-    # Create volume
-    volume = VolumeClaimTemplate("vol-cephfs-bsm", ['ReadWriteMany'], '1Gi')
-    create_workflow_volume(volume)
-    volume_mount = VolumeMount("workdir", "/mnt/vol")
-    
-    couler.set_dependencies(lambda: Step(volume_mount), dependencies=None)
-    couler.set_dependencies(lambda: Stepone(volume_mount), dependencies=["first"])
-    couler.set_dependencies(lambda: Steptwo(volume_mount), dependencies=["first"])
-    
+      volumeMounts:
+      - name: workdir
+        mountPath: /mnt/vol
+  
 
-opendata()
-submitter = ArgoSubmitter(namespace="argo")
-couler.run(submitter=submitter)
+
+
+  
+
